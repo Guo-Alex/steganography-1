51083d23396a8676386269b1753dd247bab829ba
rex-gentium <budowniczy.zamyslow@gmail.com>
2018-02-06 02:25:24 +0500

    Added images to work with, changed paths to relative

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,27 +10,28 @@ import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedImage image = readImage("D:/dump/flowerRGB.png");
-        BufferedImage mask = readImage("D:/dump/mask.bmp");
+        String currentDir = System.getProperty("user.dir");
+        BufferedImage image = readImage(currentDir + "/img/flowerRGB.png");
+        BufferedImage mask = readImage(currentDir + "/img/mask.bmp");
         /* BIT PLANES */
         BitPlanedImage imageBitPlanes = new BitPlanedImage(image);
         BitPlane maskBitPlane = new BitPlane(mask);
-        BitPlanedImage watermarkedBitPlanes = BitPlanedImage.addWatermark(imageBitPlanes, maskBitPlane, 3);
+        BitPlanedImage watermarkedBitPlanes = BitPlanedImage.addWatermark(imageBitPlanes, maskBitPlane, 1);
         BufferedImage watermarked = watermarkedBitPlanes.toBufferedImage();
         for (int i = 0; i < imageBitPlanes.getDepth(); ++i)
-            writeImage(imageBitPlanes.getPlane(i).toBufferedImage(), "D:/dump/" + i + ".bmp");
-        writeImage(watermarkedBitPlanes.getPlane(1).toBufferedImage(), "D:/dump/altered.bmp");
-        writeImage(watermarked, "D:/dump/watermarked.png");
+            writeImage(imageBitPlanes.getPlane(i).toBufferedImage(), currentDir + "/img/" + i + ".bmp");
+        writeImage(watermarkedBitPlanes.getPlane(1).toBufferedImage(), currentDir + "/img/altered.bmp");
+        writeImage(watermarked, currentDir + "/img/watermarked.png");
 
-        BitPlane resolvedMark = BitPlanedImage.resolveWatermark(watermarkedBitPlanes, imageBitPlanes, 3);
-        writeImage(resolvedMark.toBufferedImage(), "D:/dump/resolved.bmp");
+        BitPlane resolvedMark = BitPlanedImage.resolveWatermark(watermarkedBitPlanes, imageBitPlanes, 1);
+        writeImage(resolvedMark.toBufferedImage(), currentDir + "/img/resolved.bmp");
 
         /* QUANTUM */
         BufferedImage q_watermarked = QIM.addWatermark(image, mask, 10, true);
-        writeImage(q_watermarked, "D:/dump/qim_watermarked.png");
+        writeImage(q_watermarked, currentDir + "/img/qim_watermarked.png");
 
         BufferedImage q_resolvedMark = QIM.resolveWatermark(q_watermarked, 10);
-        writeImage(q_resolvedMark, "D:/dump/qim_resolved.bmp");
+        writeImage(q_resolvedMark, currentDir + "/img/qim_resolved.bmp");
     }
 
     private static void writeImage(BufferedImage image, String path) {
@@ -47,7 +48,6 @@ public class Main {
         try {
             InputStream imgInpStream = Files.newInputStream(Paths.get(path));
             BufferedImage image = ImageIO.read(imgInpStream);
-            prepareImage(image);
             return image;
         } catch (IOException e) {
             System.out.println("Failed to open file " + path);
@@ -56,13 +56,4 @@ public class Main {
         return null;
     }
 
-    private static void prepareImage(BufferedImage image) {
-        if (image.getType() != BufferedImage.TYPE_BYTE_GRAY)
-            return;
-        for (int y = 0; y < image.getHeight(); ++y)
-            for (int x  = 0; x < image.getWidth(); ++x) {
-                int rgb = image.getRGB(x, y) & 0xFF; // from 0 to 255
-                image.setRGB(x, y, new Color(rgb, rgb, rgb).getRGB());
-            };
-    }
 }
